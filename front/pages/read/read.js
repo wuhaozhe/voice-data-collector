6be6ac67fe63f9d4127400b0bab282c969fca681
baofff <2978777543@qq.com>
2018-07-29 18:09:09 +0800

    read OK

--- front/pages/read/read.js
+++ front/pages/read/read.js
@@ -10,35 +10,26 @@ Page({
     TheText: '',
     emotionAfter: '',
     TheEmotion: '',
-    current_tag: null,
-    uplo_start: null,
-    uplo_finish: null,
-    submitDisable: false,
     src: '',
     submitStat: 'init',
-    recordDisable: true,
   },
   //事件处理函数
   start: function () {
     //开始录音
     console.log('start da')
     this.setData({
-      current_tag: 1,
-      uplo_start: 0,
-      uplo_finish: 0,
+      submitStat: 'recording',
     })
     this.recorderManager.start({
       sampleRate: 16000
     });
   },
   goback: function () {
-    //开始录音
     console.log('goback da')
     this.setData({
-      current_tag: null,
+      submitStat: 'record',
     })
   },
-  //停止录音
   stop: function () {
     console.log('stop da')
     this.setData({
@@ -78,7 +69,7 @@ Page({
   uplo: function () {
     var that = this;
     this.setData({
-      uplo_start: 1,
+      submitStat: 'wait',
     })
     wx.uploadFile({
       url: 'https://hcsi.cs.tsinghua.edu.cn/upload_audio',//开发者文件上传地址
@@ -92,27 +83,22 @@ Page({
         const url = JSON.parse(res.data);//将这个url提交保存
         console.log('yes')
         console.log(that.data.src)
-        wx.showToast({
-          title: '上传成功',
-          icon: 'succes',
-          duration: 1000,
-          mask: true
-        })
+
         wx.request({
           url: 'https://hcsi.cs.tsinghua.edu.cn/datagen',
           method: 'GET',
           success: function (res) {
             //console.log(that.data.TheText)
-            that.setData({
-              TheText: res.data.text,
-              TheEmotion: res.data.emotion
-
-            })
+            setTimeout(()=>{
+              that.setData({
+                TheText: res.data.text,
+                TheEmotion: res.data.emotion,
+                submitStat: 'record',
+              })
+            }, 1000)
+            
             //console.log(that.data.TheText)
           }
-        })
-        this.setData({
-          current_tag: null,
         })
       },
       fail: res => {
@@ -121,6 +107,7 @@ Page({
       },
     });
   },
+
   onShow: function () {
     var that = this;
     
@@ -135,8 +122,7 @@ Page({
             TheText: res.data.text,
             emotionAfter: '的感情阅读下方文本:',
             TheEmotion: res.data.emotion,
-            recordDisable: false,
-            submitStat: 'submit',
+            submitStat: 'record',
           })
         }
       })
@@ -152,7 +138,8 @@ Page({
       // 停止录音之后，把录取到的音频放在res.tempFilePath
       console.log('stop dfffa')
       that.setData({
-        src: res.tempFilePath
+        src: res.tempFilePath,
+        submitStat: 'submit',
       })
       console.log(res.tempFilePath)
     });
